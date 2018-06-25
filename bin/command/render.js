a36b23bc6cb67c6433ecf9941725d6ca4213499c
liqiang0335@qq.com <mm020318@2>
2018-06-25 11:22:48 +0800

    add multi threading process

--- bin/command/render.js
+++ bin/command/render.js
@@ -21,40 +21,54 @@ const parseConfig = context => {
 };
 
 const render = async (browser, context, config) => {
-  const { createCleanPage, writeFile, exists, mkdir } = context;
-  const { dist, url, target, handler } = config;
+  return new Promise(async resolve => {
+    const { createCleanPage, writeFile, exists, mkdir } = context;
+    const { url, target, handler } = config;
 
-  // get Content from page
-  const page = await createCleanPage({ browser });
-  page.setViewport({ width: 1400, height: 900 });
-  await page.goto(url, { waitUntil: "domcontentloaded" });
-  await page.waitFor(2000);
-  const html = await page.evaluate(f => {
-    const content = document.querySelector("html").outerHTML;
-    return Promise.resolve(content);
+    // get Content from page
+    const page = await createCleanPage({ browser });
+    page.setViewport({ width: 1400, height: 900 });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await page.waitFor(2000);
+    const html = await page.evaluate(f => {
+      const content = document.querySelector("html").outerHTML;
+      return Promise.resolve(content);
+    });
+
+    await writeFile(target, handler(html));
+    console.log(`>>> render ${url} done`);
+    resolve();
   });
-
-  // write Content
-  if (!(await exists(dist))) {
-    await mkdir(dist);
-  }
-  await writeFile(target, handler(html));
-  console.log(`>>> render ${url} done`);
-  await page.close();
 };
 
 const printLine = msg => {
   console.log(`---------------- ${msg} -----------------`);
 };
 
+const prrintDuration = startTime => {
+  const duration = (Date.now() - startTime) / 1000;
+  console.log(`------------------------`);
+  console.log(`>>> [ END ] : ${duration}`);
+};
+
 module.exports = async context => {
+  const { type } = context;
   const configs = parseConfig(context);
   const browser = await puppeteer.launch({ headless: true });
+  const startTime = Date.now();
   printLine("BEGIN");
-  for (var i = 0; i < configs.length; i++) {
-    const config = configs[i];
-    await render(browser, context, config);
+
+  if (type === "queue") {
+    for (var i = 0; i < configs.length; i++) {
+      const config = configs[i];
+      await render(browser, context, config);
+    }
+    prrintDuration(startTime);
+  } else {
+    // multi threading
+    const task = configs.map(config => render(browser, context, config));
+    await Promise.all(task).then(() => prrintDuration(startTime));
   }
-  printLine("END");
+
   browser.close();
 };
